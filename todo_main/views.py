efa912cac473b8a56a939b7126e29075b493f320
Raj Patel <rajpatel@192.168.1.4>
2024-03-09 00:17:03 +0530

    todo app final

--- todo_main/views.py
+++ todo_main/views.py
@@ -3,7 +3,11 @@ from todo.models import Task
 def home(request):
     tasks = Task.objects.filter(is_compeleted=False).order_by('-updated_at')
     #it is ascending if -updated_at then descending order
+    compeleted_tasks = Task.objects.filter(is_compeleted=True)
+    
     context = {
         'tasks':tasks,
+        'compeleted_tasks':compeleted_tasks,
+
     }
     return render(request,'home.html',context)
